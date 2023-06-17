16557d4452672d8c241e0d618d749b448f9d032a
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-06-17 13:14:01 +0100

    Added app.get for lowercase strings

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const { sayHello, uppercase, lowercase } = require('./lib/strings');
 const app = express();
 
 app.get('/strings/hello/:name', (req, res) => {
-  res.json({ result: uppercase(req.params.string) });
+  res.json({ result: sayHello(req.params.string) });
 });
 
 app.get('/strings/upper/:string', (req, res) => {
