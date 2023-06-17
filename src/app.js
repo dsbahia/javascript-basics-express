b2c55b330a35b0172b0ab86d02bf1b9bce479623
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-06-17 13:31:11 +0100

    Added app.get for first-characters string without query parameters

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { sayHello, uppercase, lowercase } = require('./lib/strings');
+const { sayHello, uppercase, lowercase, firstCharacter } = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:name', (req, res) => {
-  res.json({ result: sayHello(req.params.string) });
+  res.json({ result: sayHello(req.params.name) });
 });
 
 app.get('/strings/upper/:string', (req, res) => {
@@ -13,6 +13,10 @@ app.get('/strings/upper/:string', (req, res) => {
 
 app.get('/strings/lower/:string', (req, res) => {
   res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.json({ result: firstCharacter(req.params.string) });
 });
 
 module.exports = app;
