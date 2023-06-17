3b139b7df1b387e04bd50bff75749d4bb082eec6
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-06-17 13:04:36 +0100

    Added app.get for uppercase strings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { sayHello, uppercase } = require('./lib/strings');
 
 const app = express();
 
-app.get('/strings/hello/world', (req, res) => {
-  res.status(200).send({ result: 'Hello, world!' });
+app.get('/strings/hello/:name', (req, res) => {
+  const { name } = req.params;
+  const greeting = sayHello(name);
+  res.json({ result: greeting });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
 });
 
 module.exports = app;
